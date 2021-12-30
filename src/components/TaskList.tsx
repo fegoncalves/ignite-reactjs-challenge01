c5ffecb89df8a0c39b146e232dd9fdea0bf3710b
Fernanda Gonçalves <dev.fegoncalves@gmail.com>
2021-12-30 19:22:31 -0300

    Criado filtro para remover os ítens das tasks

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -33,6 +33,8 @@ export function TaskList() {
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTask = tasks.filter(task => task.id !== id);
+    setTasks(filteredTask)
   }
 
   return (
